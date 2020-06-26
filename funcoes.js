7c9c3480567c15381654788dfb1e31109b2c437e
WagnerNasc <wagnerjr.nascimento10@gmail.com>
2020-06-25 23:19:40 -0300

    Primeiro projeto pizzaria

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+function redeSocial1(tipo1) {
+	document.getElementById("rede1").src = "img/" + tipo1 + ".png";
+}
+function redeSocial2(tipo2) {
+	document.getElementById("rede2").src = "img/" + tipo2 + ".png";
+}
+function mudaRede(image) {
+	document.getElementById("icone").src = "img/" + tipo3 + "png";
+}
