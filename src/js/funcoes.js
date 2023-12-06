3607714099d202b89e014be5420bd66dc02904ab
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2023-12-06 13:28:47 -0300

    fix: changed project

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -1,9 +1,9 @@
 function redeSocial1(tipo1) {
-	document.getElementById("rede1").src = "img/" + tipo1 + ".png";
+	document.getElementById("rede1").src = "../../assets" + tipo1 + ".png";
 }
 function redeSocial2(tipo2) {
-	document.getElementById("rede2").src = "img/" + tipo2 + ".png";
+	document.getElementById("rede2").src = "../../assets" + tipo2 + ".png";
 }
 function mudaRede(image) {
-	document.getElementById("icone").src = "img/" + tipo3 + "png";
+	document.getElementById("icone").src = "../../assets" + tipo3 + "png";
 }
